3daca3d814551f09bc2ca9d3215622323e735650
suraj bhide <suraj bhide>
2017-03-12 22:50:59 -0500

    after adding browsefity and few other things

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,21 @@ var gulp = require("gulp");
 var connect = require("gulp-connect");
 // open a url in web browser
 var open = require("gulp-open");
+// JS bundler
+var browserify = require("browserify");
+// jsx to js
+var reactify = require("reactify");
+// use conventional text streams with gulp
+var source = require("vinyl-source-stream");
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
+        js: './src/**/.js',
         dist: './dist',
+        mainJS: './src/main.js',
     },
 };
 
@@ -38,4 +46,19 @@ gulp.task('html', function(){
         .pipe(connect.reload());
 });
 
-gulp.task('default',['html', 'open']);
+gulp.task('js', function(){
+    browserify(config.paths.mainJS)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function(){
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default',['html', 'js', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,3 @@
+var App = console.log('Hello from browserify');
+
+module.exports = App;
