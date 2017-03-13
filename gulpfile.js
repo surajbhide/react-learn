f479b482a494e26b2d879aa982a5f934c3b4838d
suraj bhide <suraj bhide>
2017-03-12 22:29:58 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var gulp = require("gulp");
+// runs a local dev server
+var connect = require("gulp-connect");
+// open a url in web browser
+var open = require("gulp-open");
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        dist: './dist',
+    },
+};
+
+// start a local dev server
+gulp.task('connect', function(){
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true,
+    });
+});
+
+gulp.task('open',['connect'], function(){
+    gulp.src('dist/index.html')
+        .pipe(open({
+            uri: config.devBaseUrl + ':' + config.port + '/'
+        }));
+});
+
+gulp.task('html', function(){
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('default',['html', 'open']);
