5b85f32e2753b1e91a8659751229b0d0511cef2e
suraj bhide <suraj bhide>
2017-03-14 12:01:17 -0500

    added redirect and notfound routes

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,16 @@ var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
 
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
         <DefaultRoute handler={require('./components/homePage')}></DefaultRoute>
         <Route name="authors" handler={require('./components/authors/authorPage')}></Route>
         <Route name="about" handler={require('./components/about/aboutPage')}></Route>
+        <NotFoundRoute handler={require('./components/notFoundPage')}></NotFoundRoute>
+        <Redirect from="about-us" to="about"></Redirect>
     </Route>
 );
 
